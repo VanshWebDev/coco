1cac3a5121b23d242b6a3a99b14ca6641ddad0cb
pranavverma0311 <pranavverma114@gmail.com>
2025-09-26 12:53:33 +0530

    changed logo link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                <img style={{ height: "30px", width: "30px", marginTop: '10px' }} src="../../asset/logo.png" alt="logo" />
+                <img style={{ height: "30px", width: "30px", marginTop: '10px' }} src="https://res.cloudinary.com/dmjnq3caw/image/upload/v1758871322/IMG-20250926-WA0000_ic70lo.jpg" alt="logo" />
                 <p><b>Greenly ☘</b></p>
             </div>
 
